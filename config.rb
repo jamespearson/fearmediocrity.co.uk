228e381dfc15d2f7de4493350085b5ed7bf8cf20
James Pearson <james.pearson@thinkingincode.com>
2014-01-17 19:47:57 +0000

    Initial Config

--- config.rb
+++ config.rb
@@ -1,71 +1,94 @@
 require "active_support/all"
 ###
-# Compass
+# Blog settings
 ###
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
+# Time.zone = "UTC"
 #
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
+#sprockets.append_path '/source/js/plugins/'
+activate :asset_hash
 
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
 
-###
-# Helpers
-###
+activate  :blog do |blog|
+  # blog.prefix = "blog"
+  blog.permalink = ":year/:month/:day/:title/index.html"
+  # blog.sources = ":year-:month-:day-:title.html"
+  # blog.taglink = "tags/:tag.html"
+  blog.layout = "article"
+
+  blog.summary_separator = /(READMORE)/
+  blog.summary_length = 250
+  # blog.year_link = ":year.html"
+  # blog.month_link = ":year/:month.html"
+  # blog.day_link = ":year/:month/:day.html"
+  blog.default_extension = ".md"
+
+  blog.sources = "posts/:year-:month-:day-:title.html"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/:num"
+end
+
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = 'fearmediocrity-production' # The name of the S3 bucket you are targetting. This is globally unique.
+  s3_sync.region                     = 'eu-west-1'     # The AWS region for your bucket.
+  s3_sync.delete                     = true # We delete stray files by default.
+  s3_sync.prefer_gzip                = true
+  s3_sync.path_style                 = true
+  #s3_sync.after_build                = true
+end
+
+activate  :syntax,
+          :element => "pre>code"
+
+
+page "/google491a2a3f09d30d76.html", :directory_index => false
+page "robots.txt", :layout => false
+page "humans.txt", :layout => false
+page "/feed.xml", :layout => false
+
+default_caching_policy max_age:(60 * 60 * 24 * 365)
 
 # Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
+activate :automatic_image_sizes
 
-# Reload the browser automatically whenever files change
-# activate :livereload
+set :layouts_dir, '_layouts'
+set :partials_dir, '_partials'
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+set :css_dir, 'css'
+set :images_dir, 'img'
+set :js_dir, 'js'
 
-set :css_dir, 'stylesheets'
 
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true, :smartypants => true
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
 
+  activate :asset_host, host: 'http://testassets.fearmediocrity.co.uk'
+
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  activate :minify_html
+  
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :cache_buster
 
   # Use relative URLs
   # activate :relative_assets
 
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  require "middleman-smusher"
+  activate :smusher
+
 end
