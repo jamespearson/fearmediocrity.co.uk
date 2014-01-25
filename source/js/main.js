9ac0c07a758d848a8e40679979e7401e5d61ce12
James Pearson <james.pearson@thinkingincode.com>
2014-01-25 13:06:04 +0000

    Fixed a bug setting the sprockets path for Bower too early.

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,8 @@
 //= require "plugins/plugins.js"
 
-//= require unison/js/unison.min.js
+//= require "unison/js/unison.min.js"
 
 /* jQuery Based Plugins */
-//= require jquery-pjax/jquery.pjax.js
+//= require "jquery-pjax/jquery.pjax.js"
 
 //= require "pjax-controller"
